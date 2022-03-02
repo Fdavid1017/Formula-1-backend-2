c62496afeafa4c06649c3c9370185d0ba020d256
Fábry Dávid <fabry.david@developit.hu>
2022-03-02 10:11:59 +0100

    circuit details removed map

--- resources/circuit_infos.py
+++ resources/circuit_infos.py
@@ -7,5 +7,5 @@ from helpers.load_json import load_json
 class CircuitInfos(Resource):
     def get(self, circuit_id):
         circuit = get_circuit(circuit_id)
-        circuit['map'] = load_json(f'helpers/circuits/gjson_data/{circuit["gjson_map"]}')
+        # circuit['map'] = load_json(f'helpers/circuits/gjson_data/{circuit["gjson_map"]}')
         return circuit
